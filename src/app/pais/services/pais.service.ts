b6255223ff83ee2c04ee917f5b9c0990f18cdef4
Leyder Quintero <lele.quinter@gmail.com>
2022-09-26 21:45:22 -0500

    Tipado de la petición de RestCountries

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { Country } from '../interfaces/pais.interface';
 // import { catchError } from 'rxjs/operators';
 
 @Injectable({
@@ -12,11 +13,11 @@ export class PaisService {
 
   constructor(private http: HttpClient) { }
 
-  buscarPais(termino: string): Observable<any> {
+  buscarPais(termino: string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/name/${termino}`
 
-    return this.http.get(url);
+    return this.http.get<Country[]>(url);
       // .pipe(
       //   catchError(err => of([]))
       // );
