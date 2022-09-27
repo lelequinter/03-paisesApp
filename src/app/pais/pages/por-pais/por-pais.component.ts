194065bd8a1e325ce40c3bbe56120d5852de4e6b
Leyder Quintero <lele.quinter@gmail.com>
2022-09-26 21:32:00 -0500

    Manejo de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -12,11 +12,20 @@ export class PorPaisComponent {
   constructor(private PaisService: PaisService) { }
 
   termino: string = '';
+  hayError: boolean = false;
 
   buscar() {
     console.log(this.termino);
+    this.hayError = false
+
+    const observer = {
+      next: (x: any) => console.log('Observer got a next value: ', x),
+      error: (err: any) => {console.error('Observer got an error: ', err);
+                            this.hayError = true},
+    };
+
     this.PaisService.buscarPais(this.termino)
-    .subscribe(resp => console.log(resp))
+      .subscribe(observer)
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+// import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,10 @@ export class PaisService {
     const url = `${this.apiUrl}/name/${termino}`
 
     return this.http.get(url);
+      // .pipe(
+      //   catchError(err => of([]))
+      // );
+
 
   }
 
