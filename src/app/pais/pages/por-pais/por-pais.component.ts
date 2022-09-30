47b76e8df1582584b6ea04764e4c7bee3f87e22e
Leyder Quintero <lele.quinter@gmail.com>
2022-09-29 21:43:38 -0500

    Mostrar sugerencias al escribir - autocomplete

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,8 +5,11 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [`
+    li{
+      cursor: pointer;
+    }
+  `]
 })
 export class PorPaisComponent {
 
@@ -14,29 +17,44 @@ export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
-  paises : Country[] = [];
+  paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   buscar(termino: string) {
+    this.mostrarSugerencias = false;
+
     this.termino = termino;
     // console.log(this.termino);
     this.hayError = false;
 
     const observer = {
       next: (resp: any) => {//console.log('Observer got a next value: ', resp)
-                            this.paises = resp},
+        this.paises = resp
+      },
       error: (err: any) => {//console.error('Observer got an error: ', err);
-                            this.hayError = true;
-                            this.paises = []},
+        this.hayError = true;
+        this.paises = []
+      },
     };
 
     this.PaisService.buscarPais(this.termino)
       .subscribe(observer)
   }
 
-  sugerencias(termino: string){
+  sugerencias(termino: string) {
     this.hayError = false;
-    //Todo: Crear sugenrencias
+    this.termino = termino;
+    this.mostrarSugerencias = true;
 
+    //Todo: Crear sugenrencias
+    this.PaisService.buscarPais(termino)
+      .subscribe(paises => this.paisesSugeridos = paises.splice(0, 5),
+        (err) => this.paisesSugeridos = [])
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
   }
 
 }
