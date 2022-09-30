57c3051d4ce34a6f50d333c06a452f98dcb2bc37
Leyder Quintero <lele.quinter@gmail.com>
2022-09-29 20:07:55 -0500

    ngClass, class y [class]

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -3,14 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [`
+    .col {
+      display: flex;
+      gap: 10px
+    }
+    `]
 })
 export class PorRegionComponent implements OnInit {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  activarRegion(region: string) {
+    this.regionActiva = region;
   }
 
 }
